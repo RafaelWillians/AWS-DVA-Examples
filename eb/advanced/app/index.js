24128a9fff627324478cb4e9a350bbcc4085abb0
Rafael Silva Willians <rafael.willians@outlook.com>
2025-04-18 16:01:46 +0000

    Corrigidos codigos

--- eb/advanced/app/index.js
+++ eb/advanced/app/index.js
@@ -4,15 +4,15 @@ const path = require('path')
 const port = process.env.PORT
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));    
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/style.css'));    
+app.get('/style.css', function(req, res) {
+  res.sendFile(path.join(__dirname + '/style.css'));
 });
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/app.js'));    
+app.get('/app.js', function(req, res) {
+  res.sendFile(path.join(__dirname + '/app.js'));
 });
 
 console.log(`PLANNING TO USE PORT: ${port}`)
